6b8cf05d72db780af2a315c2ef955b8db17baafb
Iliyan <iddragnev@gmail.com>
2022-05-29 16:29:44 +0300

    Improve error handling for lib_loader

--- src/lib_loader.rs
+++ src/lib_loader.rs
@@ -19,36 +19,44 @@ extern "stdcall" {
 
 #[derive(Debug)]
 pub struct Library {
+    name: String,
     module: HModule,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(thiserror::Error, Debug, PartialEq)]
 pub enum LoadLibError {
-    LibNotFound,
-    InvalidLibName,
+    #[error("could not open library {0:?}")]
+    NotFound(String),
+    #[error("invalid library name: {0:?}")]
+    InvalidName(#[from] std::ffi::NulError),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(thiserror::Error, Debug, PartialEq)]
 pub enum GetProcError {
-    ProcNotFound,
-    InvalidProcName,
+    #[error("could not find proc {proc:?} in {lib:?}")]
+    NotFound{ proc: String, lib: String },
+    #[error("invalid proc name: {0:?}")]
+    InvalidName(#[from] std::ffi::NulError),
 }
 
 impl Library {
     pub fn new(name: &str) -> Result<Self, LoadLibError> {
-        let c_name = CString::new(name).or(Err(LoadLibError::InvalidLibName))?;
+        let c_name = CString::new(name)?;
         let l = unsafe { LoadLibraryA(c_name.as_ptr()) };
 
-        l.map(|module| Library { module })
-         .ok_or(LoadLibError::LibNotFound)
+        l.map(|module| Library { name: name.to_owned(), module })
+         .ok_or(LoadLibError::NotFound(name.to_owned()))
     }
 
     pub fn get_proc<T>(&self, name: &str) -> Result<T, GetProcError> {
-        let c_name = CString::new(name).or(Err(GetProcError::InvalidProcName))?;
+        let c_name = CString::new(name)?;
         let proc = unsafe { GetProcAddress(self.module, c_name.as_ptr()) };
 
         proc.map(|p| unsafe { transmute_copy(&p) })
-            .ok_or(GetProcError::ProcNotFound)
+            .ok_or(GetProcError::NotFound {
+                proc: name.to_owned(),
+                lib: self.name.clone()
+            })
     }
 }
 
@@ -85,13 +93,13 @@ mod test {
     #[test]
     fn load_lib_invalid_name() {
         let l = Library::new("name\0abcd");
-        assert!(matches!(l, Err(LoadLibError::InvalidLibName)));
+        assert!(matches!(l, Err(LoadLibError::InvalidName(_))));
     }
 
     #[test]
     fn load_lib_non_existing_lib() {
         let l = Library::new("non_existing_lib_name");
-        assert!(matches!(l, Err(LoadLibError::LibNotFound)));
+        assert!(matches!(l, Err(LoadLibError::NotFound(_))));
     }
 
     #[test]
@@ -103,14 +111,14 @@ mod test {
     fn get_proc_non_existent() {
         let l = Library::new(TEST_DLL_NAME).unwrap();
         let p = l.get_proc::<fn() -> ()>("NoSuchProcName");
-        assert!(matches!(p, Err(GetProcError::ProcNotFound)));
+        assert!(matches!(p, Err(GetProcError::NotFound{ proc: _, lib: _ })));
     }
     
     #[test]
     fn get_proc_invalid_name() {
         let l = Library::new(TEST_DLL_NAME).unwrap();
         let p = l.get_proc::<fn() -> ()>("proc\0name");
-        assert!(matches!(p, Err(GetProcError::InvalidProcName)));
+        assert!(matches!(p, Err(GetProcError::InvalidName(_))));
     }
 
     #[test]
